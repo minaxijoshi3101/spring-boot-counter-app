03db2831492fc60c201f6bb25e3c2ca5842e4780
Ender Culha <enderculha@gmail.com>
2020-02-01 22:07:32 +0100

    Initial commit

---
+++ src/main/java/com/enderculha/apsisassignment/ApsisAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.enderculha.apsisassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsisAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApsisAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enderculha/apsisassignment/ApsisAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enderculha.apsisassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsisAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
